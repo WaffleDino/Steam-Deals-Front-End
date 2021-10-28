d62a1290d6d9535b3c49cb5b3dd840a5ca58d77b
Brennan McCandliss <osmedia1220@gmail.com>
2021-10-28 09:23:22 -0700

    added user id to purchases

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,107 @@
-import React, {useEffect, useState} from 'react'
-import {Route, Switch} from 'react-router-dom'
-import Home from "./Home"
-import NavBar from "./NavBar"
-import Purchases from "./Purchases"
-import SavedDeals from "./SavedDeals"
+import React, { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import Purchases from "./Purchases";
+import SavedDeals from "./SavedDeals";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+  padding: "10px 20px 10px 20px",
+  margin: "10px 10px 10px 10px",
+  background: "#f1d302",
+  textDecoration: "none",
+  color: "white",
+  justifyContent: "center",
+  textAlign: "center",
+  borderRadius: "8px",
+};
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-  const [currentUser, setCurrentUser] = useState([])
+  const [currentUser, setCurrentUser] = useState([]);
+
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(0);
+
+  const saveSelectedUser = (event) => {
+    setSelectedUser(parseInt(event.target.value));
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:9292/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        console.log(users)
+      });
+  }, []);
+
+  const renderUser = () => {
+    if (!!users) {
+      return users.map((eachUser) => {
+        return <option value={eachUser.id}>{eachUser.name}</option>;
+      });
+    }
+  };
 
   // fetch URL-API
   useEffect(() => {
     fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15")
-    .then(r => r.json())
-    .then(json => setData(json))
-  }, [])
+      .then((r) => r.json())
+      .then((json) => setData(json));
+  }, []);
 
   return (
     <div className="App">
-      <NavBar />
+      <div className="nav-bar">
+        <NavLink
+          to="/"
+          exact
+          style={linkStyles}
+          className="nav-bar-route"
+          activeStyle={{
+            background: "#235789",
+          }}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/Purchases"
+          exact
+          style={linkStyles}
+          className="nav-bar-route"
+          activeStyle={{
+            background: "#235789",
+          }}
+        >
+          Purchases
+        </NavLink>
+
+        <NavLink
+          to="/SavedDeals"
+          exact
+          style={linkStyles}
+          className="nav-bar-route"
+          activeStyle={{
+            background: "#235789",
+          }}
+        >
+          Saved Deals
+        </NavLink>
+        <select onChange={saveSelectedUser}>
+          <option value="" disabled selected>
+            Select User
+          </option>
+          {renderUser()}
+        </select>
+      </div>
+
       <Switch>
         <Route exact path="/">
-          <Home data={data}/>
+          <Home data={data} selectedUser={selectedUser} />
         </Route>
         <Route exact path="/Purchases">
           <Purchases />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,87 +1,9 @@
 import React, {useState, useEffect} from "react";
-import { NavLink } from "react-router-dom";
-
-const linkStyles = {
-  padding: "10px 20px 10px 20px",
-  margin: "10px 10px 10px 10px",
-  background: "#f1d302",
-  textDecoration: "none",
-  color: "white",
-  justifyContent: "center",
-  textAlign: "center",
-  borderRadius: "8px",
-};
 
 function NavBar() {
 
-    const[users, setUsers] = useState([])
-    const[selectedUser, setSelectedUser] = useState({
-        id:0
-    })
-
-
-    const saveSelectedUser = (event) => {
-        setSelectedUser({...selectedUser, [event.target.id]: event.target.value})
-        console.log(event.target.value)
-    }
-
-    useEffect (() => {
-        fetch("http://localhost:9292/users")
-        .then(response => response.json())
-        .then(data => {
-            setUsers(data)
-        })
-    }, [])
-
-    const renderUser = () => {
-        if (!!users) {
-        return users.map((eachUser) => {
-            return (
-           <option value={eachUser.id}>{eachUser.name}</option>
-    )})}}
-
   return (
-    <div className="nav-bar">
-      <NavLink
-        to="/"
-        exact
-        style={linkStyles}
-        className="nav-bar-route"
-        activeStyle={{
-          background: "#235789",
-        }}
-      >
-        Home
-      </NavLink>
-
-      <NavLink
-        to="/Purchases"
-        exact
-        style={linkStyles}
-        className="nav-bar-route"
-        activeStyle={{
-          background: "#235789",
-        }}
-      >
-        Purchases
-      </NavLink>
-
-      <NavLink
-        to="/SavedDeals"
-        exact
-        style={linkStyles}
-        className="nav-bar-route"
-        activeStyle={{
-          background: "#235789",
-        }}
-      >
-        Saved Deals
-      </NavLink>
-            <select onChange={saveSelectedUser}>
-            <option value="" disabled selected>Select User</option>
-            {renderUser()}
-            </select>
-    </div>
+    console.log("test")
   );
 }
 
